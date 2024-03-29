0de4a7736b278d1feaddac61603a4e5820c3bb42
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2024-03-29 12:53:28 -0300

    feat: retorna prmieiras requisicoes de personagens

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,48 @@
 import './App.css';
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return <></>;
+  const [people, setPeople] = useState([]);
+
+  const fetchPeopleData = async () => {
+    try {
+      const response1 = await axios.get('https://swapi.dev/api/people/');
+      const data1 = response1.data.results;
+
+      const response2 = await axios.get('https://swapi.dev/api/people/?page=2');
+      const data2 = response2.data.results;
+
+      const allDataPeople = [...data1, ...data2];
+
+      const peopleMovie = allDataPeople.map((character) => ({
+        id: uuidv4(),
+        name: character.name,
+      }));
+
+      setPeople(peopleMovie);
+    } catch (error) {
+      console.log('Ocorreu um erro ao obter os dados:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPeopleData();
+  }, []);
+
+  return (
+    <>
+      <h1>Título do Projeto</h1>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <p>{person.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
